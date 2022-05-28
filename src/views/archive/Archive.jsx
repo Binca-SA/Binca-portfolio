249a725b603f092cf2e5b2ea602f326269b8f7f6
Binca-SA <binnas.shinaasi.asanova@gmail.com>
2022-05-28 10:55:07 +0200

    starting printing information on Archive.jsx

---
+++ src/views/archive/Archive.jsx
@@ -0,0 +1,35 @@
+import { useEffect, useState } from 'react';
+import React from 'react';
+import axios from 'axios';
+import './archive.css';
+
+function GetPhotos(){
+
+    const [archive, setArchive] = useState([]);
+
+    //getting json//
+    useEffect(() => {
+        axios.get('json/archive.json')
+        .then(response => setArchive(response.data));
+    }, [])
+
+
+    //printing images//
+    return (
+        <>
+            <section className='container-grid'>
+                <h2 className='main-titles'>a collection<br></br>of things<br></br>I do</h2>
+                {archive.map(photo => (<>
+                    <div className='container-grid__cont-photo'>
+                        <img src='{ photo.img }'></img>
+                        <p key={photo.name}>{ photo.name }</p>
+                        <p key={photo.description}>{ photo.description }</p>
+                        <p key={photo.tools}>{ photo.tools }</p>
+                    </div>
+                </>))}
+            </section>
+        </>
+    );
+}
+
+export default GetPhotos;
