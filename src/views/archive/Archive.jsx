350c458beaf4766b6ce83f725a3af37d2db6d6ee
Binca-SA <binnas.shinaasi.asanova@gmail.com>
2022-05-28 12:03:39 +0200

    images added archive.json and imported on archive.jsx

--- src/views/archive/Archive.jsx
+++ src/views/archive/Archive.jsx
@@ -21,7 +21,7 @@ function GetPhotos(){
                 <h2 className='main-titles'>a collection<br></br>of things<br></br>I do</h2>
                 {archive.map(photo => (<>
                     <div className='container-grid__cont-photo'>
-                        <img src='{ photo.img }'></img>
+                        <img key={photo.img} src={photo.img}></img>
                         <p key={photo.name}>{ photo.name }</p>
                         <p key={photo.description}>{ photo.description }</p>
                         <p key={photo.tools}>{ photo.tools }</p>
